57aa6eb3ff90c276209b03d831186477360f78fe
yt723 <131491466+yt723@users.noreply.github.com>
2025-05-27 16:23:53 -0700

    Add files via upload

---
+++ Rhapsody_CITEseq_12_pseudobulk2.R
@@ -0,0 +1,257 @@
+#### Comparison of gene expression by pseudobulk 
+
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+library(edgeR)
+library(tidyr)
+
+rm(list=ls())
+
+path = "~/Documents/UCSD_CAV/CITEseq"
+
+## IFNG expression by CD8T, CD4T, NK clusters 
+CD8T <- readRDS(file = paste0(path, "/output/4_CD8T_clusters/CD8T_clusters.rds"))
+CD4T <- readRDS(file = paste0(path, "/output/5_CD4T_clusters/CD4T_clusters.rds"))
+NK <- readRDS(file = paste0(path, "/output/7_NK_clusters/NK_clusters.rds"))
+
+CD8T$cluster <- paste0("CD8T_C",CD8T$cluster_0.3)
+cts.cd8 <- AggregateExpression(CD8T, assays = "RNA", group.by = c("cluster","SampleName"), return.seurat = F)
+cts.cd8 <- cts.cd8$RNA
+
+CD4T$cluster <- paste0("CD4T_C",CD8T$cluster_0.3)
+cts.cd4 <- AggregateExpression(CD4T, assays = "RNA", group.by = c("cluster","SampleName"), return.seurat = F)
+cts.cd4 <- cts.cd4$RNA
+
+NK$cluster <- paste0("NK_C",CD8T$cluster_0.3)
+cts.nk <- AggregateExpression(NK, assays = "RNA", group.by = c("cluster","SampleName"), return.seurat = F)
+cts.nk <- cts.nk$RNA
+
+cts <- cbind(cts.cd8, cts.cd4, cts.nk)
+colData <- data.frame(sample = colnames(cts))
+col.order <- colnames(cts)
+rownames(colData) <- colData$sample
+colData <- separate(colData, col = "sample", into = c("cluster","SampleName"), sep = "_",
+                    remove = F)
+
+meta <- distinct(CD8T@meta.data[,c("SampleName","SampleType","Library") ])
+meta$SampleName <- gsub("_","-", meta$SampleName)
+colData <- colData %>% merge(meta, by.x = "SampleName", by.y = "SampleName")
+rownames(colData) <- colData$sample
+colData <- colData[order(factor(rownames(colData), levels = col.order)),]
+
+obj <- DGEList(counts = cts, samples = colData)
+#head(obj$samples, 25)
+obj <- calcNormFactors(obj)
+log2CPM <- cpm(obj, log=TRUE)
+out <- removeBatchEffect(log2CPM, batch = obj$samples$Library)
+out <- as.data.frame(t(out))
+
+out.z <- sapply(out, function(dat) (dat - mean(dat))/sd(dat))
+rownames(out.z) <- rownames(out)
+out.z <- out.z %>% merge(colData, by = 0)
+
+dat.summary <- out.z %>% group_by(cluster) %>% 
+  summarise(IFNG = mean(IFNG), TNF = mean(TNF), GZMK = mean(GZMK), GZMB = mean(GZMB), PRF1 = mean(PRF1))
+dat.summary <- dat.summary %>% pivot_longer(cols = 2:6, names_to = "marker", values_to = "average")
+dat.summary$cluster <- gsub("-","_", dat.summary$cluster)
+dat.summary$marker <- factor(dat.summary$marker,
+                             levels = c("IFNG","TNF","GZMK","GZMB","PRF1"))
+
+# *Figure S7B
+ggplot(dat.summary, aes(y=marker, x=cluster)) +
+  geom_point(aes(colour = average), size=3) +
+  cowplot::theme_cowplot() +
+  theme(axis.text.x = element_text(size = 9, face='bold',,angle = 270, hjust = 0, vjust=0.5),
+        axis.text.y = element_text(size = 9, face='bold'),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        legend.title = element_text(size = 9), 
+        panel.background = element_blank(),
+        panel.grid = element_line(color = "gray50", linetype = 3))+
+  scale_color_gradient(low = 'blue', high = 'yellow', name = 'Pseudobulk \nexpression \nzscore') +
+  xlab("") + ylab("")
+ggsave(file = paste0(path, "/output/12_pseudobulk2/dotplot_cytotoxic_CD8_CD4_NK_clusters.png"),
+       width = 5, height = 3, units = "in", bg = 'white')
+
+## SampleType comparison 
+# CD8T 
+cts <- AggregateExpression(CD8T, assays = "RNA", group.by = c("SampleName"), return.seurat = F)
+cts <- cts$RNA
+
+col.order <- colnames(cts)
+colData <- data.frame(sample = colnames(cts))
+rownames(colData) <- colData$sample
+colData$SampleName <- colData$sample
+meta <- distinct(CD8T@meta.data[,c("SampleName", "Library", "SampleType")])
+meta$SampleName <- gsub("_","-", meta$SampleName)
+
+colData <- colData %>% merge(meta, by.x = c("SampleName"), by.y = "SampleName")
+rownames(colData) <- colData$sample
+colData <- colData[order(factor(rownames(colData), levels = col.order)),]
+
+obj <- DGEList(counts = cts, samples = colData)
+head(obj$samples, 25)
+keep <- filterByExpr(obj, group = colData$SampleName)
+summary(keep)
+obj <- obj[keep,]
+obj <- calcNormFactors(obj)
+plotMDS(obj)
+
+mm <- model.matrix(~ 0+ factor(SampleType), obj$samples)
+colnames(mm) <- c("CAV", "Normal_HTx")
+
+obj <- estimateDisp(obj, mm)
+plotBCV(obj)
+fit <- glmQLFit(obj, mm, robust = T)
+
+contr <- makeContrasts("CAV-Normal_HTx", levels = mm)
+lrt <- glmQLFTest(fit, contrast = contr)
+
+de.results <- lrt$table
+
+pval <- de.results$PValue
+names(pval) <- rownames(de.results)
+pval <- p.adjust(pval, method = 'fdr')
+de.results$fdr <- pval
+de.results <- de.results[de.results$PValue <0.05,]
+
+# *Excel file 5*
+write.csv(de.results, file = paste0(path, "/output/12_pseudobulk2/edgeR_dge_SampleType_CD8T.csv"))
+
+
+# CD4T 
+cts <- AggregateExpression(CD4T, assays = "RNA", group.by = c("SampleName"), return.seurat = F)
+cts <- cts$RNA
+
+col.order <- colnames(cts)
+colData <- data.frame(sample = colnames(cts))
+rownames(colData) <- colData$sample
+colData$SampleName <- colData$sample
+meta <- distinct(CD4T@meta.data[,c("SampleName", "Library", "SampleType")])
+meta$SampleName <- gsub("_","-", meta$SampleName)
+
+colData <- colData %>% merge(meta, by.x = c("SampleName"), by.y = "SampleName")
+rownames(colData) <- colData$sample
+colData <- colData[order(factor(rownames(colData), levels = col.order)),]
+
+obj <- DGEList(counts = cts, samples = colData)
+head(obj$samples, 25)
+keep <- filterByExpr(obj, group = colData$SampleName)
+summary(keep)
+obj <- obj[keep,]
+obj <- calcNormFactors(obj)
+plotMDS(obj)
+
+mm <- model.matrix(~ 0+ factor(SampleType), obj$samples)
+colnames(mm) <- c("CAV", "Normal_HTx")
+
+obj <- estimateDisp(obj, mm)
+plotBCV(obj)
+fit <- glmQLFit(obj, mm, robust = T)
+
+contr <- makeContrasts("CAV-Normal_HTx", levels = mm)
+lrt <- glmQLFTest(fit, contrast = contr)
+
+de.results <- lrt$table
+
+pval <- de.results$PValue
+names(pval) <- rownames(de.results)
+pval <- p.adjust(pval, method = 'fdr')
+de.results$fdr <- pval
+de.results <- de.results[de.results$PValue <0.05,]
+
+# *Excel file 5*
+write.csv(de.results, file = paste0(path, "/output/12_pseudobulk2/edgeR_dge_SampleType_CD4T.csv"))
+
+# Bcell
+Bcell <- readRDS(file = paste0(path, "/output/6_Bcell_clusters/Bcell_clusters.rds"))
+cts <- AggregateExpression(Bcell, assays = "RNA", group.by = c("SampleName"), return.seurat = F)
+cts <- cts$RNA
+
+col.order <- colnames(cts)
+colData <- data.frame(sample = colnames(cts))
+rownames(colData) <- colData$sample
+colData$SampleName <- colData$sample
+meta <- distinct(Bcell@meta.data[,c("SampleName", "Library", "SampleType")])
+meta$SampleName <- gsub("_","-", meta$SampleName)
+
+colData <- colData %>% merge(meta, by.x = c("SampleName"), by.y = "SampleName")
+rownames(colData) <- colData$sample
+colData <- colData[order(factor(rownames(colData), levels = col.order)),]
+
+obj <- DGEList(counts = cts, samples = colData)
+head(obj$samples, 25)
+keep <- filterByExpr(obj, group = colData$SampleName)
+summary(keep)
+obj <- obj[keep,]
+obj <- calcNormFactors(obj)
+plotMDS(obj)
+
+mm <- model.matrix(~ 0+ factor(SampleType), obj$samples)
+colnames(mm) <- c("CAV", "Normal_HTx")
+
+obj <- estimateDisp(obj, mm)
+plotBCV(obj)
+fit <- glmQLFit(obj, mm, robust = T)
+
+contr <- makeContrasts("CAV-Normal_HTx", levels = mm)
+lrt <- glmQLFTest(fit, contrast = contr)
+
+de.results <- lrt$table
+
+pval <- de.results$PValue
+names(pval) <- rownames(de.results)
+pval <- p.adjust(pval, method = 'fdr')
+de.results$fdr <- pval
+de.results <- de.results[de.results$PValue <0.05,]
+
+# *Excel file 5*
+write.csv(de.results, file = paste0(path, "/output/12_pseudobulk2/edgeR_dge_SampleType_Bcell.csv"))
+
+# NK
+cts <- AggregateExpression(NK, assays = "RNA", group.by = c("SampleName"), return.seurat = F)
+cts <- cts$RNA
+
+col.order <- colnames(cts)
+colData <- data.frame(sample = colnames(cts))
+rownames(colData) <- colData$sample
+colData$SampleName <- colData$sample
+meta <- distinct(NK@meta.data[,c("SampleName", "Library", "SampleType")])
+meta$SampleName <- gsub("_","-", meta$SampleName)
+
+colData <- colData %>% merge(meta, by.x = c("SampleName"), by.y = "SampleName")
+rownames(colData) <- colData$sample
+colData <- colData[order(factor(rownames(colData), levels = col.order)),]
+
+obj <- DGEList(counts = cts, samples = colData)
+head(obj$samples, 25)
+keep <- filterByExpr(obj, group = colData$SampleName)
+summary(keep)
+obj <- obj[keep,]
+obj <- calcNormFactors(obj)
+plotMDS(obj)
+
+mm <- model.matrix(~ 0+ factor(SampleType), obj$samples)
+colnames(mm) <- c("CAV", "Normal_HTx")
+
+obj <- estimateDisp(obj, mm)
+plotBCV(obj)
+fit <- glmQLFit(obj, mm, robust = T)
+
+contr <- makeContrasts("CAV-Normal_HTx", levels = mm)
+lrt <- glmQLFTest(fit, contrast = contr)
+
+de.results <- lrt$table
+
+pval <- de.results$PValue
+names(pval) <- rownames(de.results)
+pval <- p.adjust(pval, method = 'fdr')
+de.results$fdr <- pval
+de.results <- de.results[de.results$PValue <0.05,]
+
+# *Excel file 5*
+write.csv(de.results, file = paste0(path, "/output/12_pseudobulk2/edgeR_dge_SampleType_NK.csv"))
+
+
+# *** end of pseudobulk analysis2 ***
---
+++ Rhapsody_CITEseq_13_CD8T_gated_proportion.R
@@ -0,0 +1,70 @@
+#### CD8 T cell proportion comparison determined by gates 
+
+library(Seurat)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+rm(list=ls())
+
+path = "~/Documents/UCSD_CAV/CITEseq"
+
+#load the data
+CD8T <- readRDS(file = paste0(path, "/output/4_CD8T_clusters/CD8T_clusters.rds"))
+#CD161: 2.5
+#CCR7: 1.1
+#CD27: 1.9
+#CD38: 1.3
+#HLA.DR: 2.4
+#CD56: 2.7 
+
+df <- CD8T@assays$integratedADT@data %>% t() %>% as.data.frame()
+df <- df[,c("CD161","CCR7","CD27","CD38","HLA.DR","CD56")]
+df$cluster <- "others"
+df$cluster[df$CD161 >2.5] <- "C6"
+df$cluster[df$CD161 <=2.5 & df$CCR7 >1.1 & df$CD27 >1.9] <- "C3"
+df$cluster[df$CD161 <=2.5 & df$CCR7 <=1.1 & df$CD27 >1.9 & df$CD38 <=1.3 & df$HLA.DR <=2.4] <- "C7"
+df$cluster[df$CD161 <=2.5 & df$CCR7 <=1.1 & df$CD27 >1.9 & df$CD38 <=1.3 & df$HLA.DR >2.4] <- "C2"
+df$cluster[df$CD161 <=2.5 & df$CCR7 <=1.1 & df$CD27 >1.9 & df$CD38 >1.3 & df$HLA.DR >2.4] <- "C4"
+df$cluster[df$CD161 <=2.5 & df$CCR7 <=1.1 & df$CD27 <=1.9 & df$CD56 <=2.7] <- "C1"
+df$cluster[df$CD161 <=2.5 & df$CCR7 <=1.1 & df$CD27 <=1.9 & df$CD56 >2.7] <- "C5"
+
+table(df$cluster)
+all(rownames(CD8T@meta.data)==rownames(df)) 
+CD8T$gate_cluster <- df$cluster
+
+# gated proportion comparison
+df_prop <- CD8T@meta.data %>% group_by(gate_cluster, SampleName, SampleType) %>% summarise(num=n())
+df_prop <- df_prop %>% pivot_wider(names_from = 'gate_cluster', values_from = 'num', values_fill = 0)
+
+mat_prop <- df_prop[, 3:ncol(df_prop)] %>% as.matrix()
+mat_prop <- round(prop.table(mat_prop, margin = 1) *100, 2)
+df_prop[,3:ncol(df_prop)] <- mat_prop 
+
+write.csv(df_prop, file = paste0(path,"/output/13_gated_proportion/CD8T_gate_cluster_proportion_patient.csv"), row.names = F) 
+
+## similarity between the unsupervised and gated clusters 
+df <- CD8T@meta.data %>% select(cluster_name0.3, gate_cluster)
+df_sum <- df %>% group_by(cluster_name0.3, gate_cluster) %>% summarise(num=n())
+df_sum <- df_sum %>% group_by(gate_cluster) %>% mutate(total = sum(num), prop = round(num/total,2))
+df_mat <- df_sum[,c("cluster_name0.3","gate_cluster","prop")]
+df_mat <- df_mat %>% pivot_wider(names_from = cluster_name0.3, values_from = prop, values_fill = 0)
+df_mat <- df_mat %>% pivot_longer(cols = 2:8, names_to = "cluster_name0.3", values_to = "prop")
+df_mat$gate_cluster <- paste0("gate_", df_mat$gate_cluster)
+
+ggplot(df_mat, aes(y=cluster_name0.3, x=gate_cluster, fill = prop)) +
+  geom_tile(color='black', linewidth = 0.5) +
+  geom_text(aes(label=prop)) +
+  scale_fill_gradient(high = 'red', low = 'white',
+                      name = 'Cluster\noverlap') +
+  theme_minimal() +
+  theme(axis.text = element_text(size = 16, face = 'bold'),
+        axis.text.x = element_text(angle = 30, vjust = 1, hjust = 0.75),
+        legend.title = element_text(size=16))+
+  xlab("") + ylab("")+
+  coord_fixed()  
+ggsave(file= paste0(path,"/output/13_gated_proportion/heatmap_CD8T_gate_cluster_overlap.png"),
+       height = 6, width = 7, units = 'in', bg = 'white')
+
+
+# *** end of CD8T gated clusters ***
